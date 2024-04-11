19e573f18231ee9a683f8c6d5feb201cd0d22330
Ghaieth Grami <ghaieth.grami06@gmail.com>
2024-04-11 16:18:40 +0200

    inital commit

---
+++ src/components/Sections/HeroSection.js
@@ -0,0 +1,93 @@
+import React from 'react';
+import TheButton from '../Ui/TheButton';
+import classes from './HeroSection.module.css';
+import { Row, Col, Container } from 'react-bootstrap';
+import { Link } from 'react-scroll';
+import FoodImage from '../../assets/image/FoodImage.webp';
+import FastDelivery from '../../assets/image/fast.png';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const HeroSection = () => {
+  return (
+    <section id='hero' className={classes.heroSection}>
+      <Container>
+        <div
+          style={{ width: '100%', backgroundColor: 'var(--primarybackground)' }}
+        >
+          <Row className={`${classes.row} mx-auto`}>
+            <Col
+              lg={6}
+              className='p-0'
+              data-aos='fade-right'
+              data-aos-easing='ease-out'
+              data-aos-duration='1500'
+            >
+              <div className={classes.text__div}>
+                <h1>Taste Your Favorite Food</h1>
+                <p>
+                  We are just a click away from you when you crave for delicious
+                  fast food
+                </p>
+
+                <Link
+                  className={classes.order_button}
+                  to='dishes'
+                  spy={true}
+                  smooth={true}
+                  offset={-50}
+                  duration={500}
+                >
+                  <TheButton>Order Now</TheButton>
+                </Link>
+              </div>
+            </Col>
+            <Col lg={6} className='p-0'>
+              <div className={`ms-auto`}>
+                <img
+                  className={classes.food__image}
+                  src={FoodImage}
+                  fluid='true'
+                  alt='Food pic'
+                  data-aos='fade-left'
+                  data-aos-easing='ease-out'
+                  data-aos-duration='1500'
+                ></img>
+              </div>
+            </Col>
+          </Row>
+        </div>
+        <Row
+          data-aos='fade-up'
+          data-aos-easing='ease-out'
+          data-aos-duration='700'
+        >
+          <div className={classes.number__properties}>
+            <div>
+              <img src={FastDelivery} alt='delivery' className={classes.img} />
+              <p className={classes.number__properties__digit}>Fast Delivery</p>
+              <p className={classes.number__properties__text}>
+                Delivery to your home within 1 Hour of your ordering
+              </p>
+            </div>
+            <div className='mx-4'>
+              <p className={classes.number__properties__digit}>Fresh Food</p>
+              <p className={classes.number__properties__text}>
+                Your food will be prepared with fresh ingredients
+              </p>
+            </div>
+            <div>
+              <p className={classes.number__properties__digit}>Free Delivery</p>
+              <p className={classes.number__properties__text}>
+                Food Delivery is absolutely free.
+                <br />
+                Just order and enjoy
+              </p>
+            </div>
+          </div>
+        </Row>
+      </Container>
+    </section>
+  );
+};
+
+export default HeroSection;
